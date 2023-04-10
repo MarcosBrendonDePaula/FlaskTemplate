435e3095da7f3f1fd9b25df119592188f4450725
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2023-04-09 21:59:59 -0300

    template basico

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_from_directory, render_template
+app = Flask(__name__,template_folder='web/templates')
+
+@app.route('/public/<path:filename>')
+def public(filename):
+    return send_from_directory('web/public', filename)
+
+@app.route("/")
+def hello_world():
+    return render_template("teste.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
