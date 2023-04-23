d0f459550024c17a168a8a2708377b094f5dcac1
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2023-04-23 15:14:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ from flask import Flask, send_from_directory, render_template
 
 app = Flask(__name__,template_folder='web/templates')
 
+@app.context_processor
+def utility_processor():
+    def now():
+        return datetime.now()
+    def old(field_name):
+        return request.form.get(field_name) if field_name in request.form else ''
+    return dict(now=now, validation={}, error_bag={}, old=old)
+
 @app.route('/public/<path:filename>')
 def public(filename):
     return send_from_directory('web/public', filename)
